2a2ec776be7a7c903ff50242cf477c4128069175
GurpreetAndroid <java.preetsingh@gmail.com>
2024-09-18 10:46:29 +0530

    Add image upload validation

--- lib/image_upload.dart
+++ lib/image_upload.dart
@@ -1,24 +1,21 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-
-import 'package:image/image.dart' as img;
 import 'package:http/http.dart' as http;
 import 'package:permission_handler/permission_handler.dart';
+import 'image_util.dart';
 
 class ImageUpload extends StatefulWidget {
-  const ImageUpload({Key? key}) : super(key: key);
+  const ImageUpload({super.key});
 
   @override
   _ImageUploadState createState() => _ImageUploadState();
 }
 
 class _ImageUploadState extends State<ImageUpload> {
-  late File _image;
+    File? selectedImage;
   ImagePicker imagePicker = ImagePicker();
-  var placeholder = AssetImage('assets/img_default.png');
-  bool capture = false;
+  bool isImageSelected = false;
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +45,7 @@ class _ImageUploadState extends State<ImageUpload> {
                     Text("Camera", style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w400,),)
                   ]),
                   onPressed: () => {
-                    _imageFromCamera(),
+                    pickFromCamera(),
                   },
                 ),
               ),
@@ -70,22 +67,22 @@ class _ImageUploadState extends State<ImageUpload> {
                     Text("Gallery", style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w400,),)
                   ]),
                   onPressed: () => {
-                    _imageFromGallery(),
+                    pickFromGallery(),
                   },
                 ),
               ),
             ]),
             const SizedBox(height: 20,),
             Container(
-                child: capture == true
+                child: isImageSelected == true
                     ? CircleAvatar(
                         radius: MediaQuery.of(context).size.width / 5,
-                        backgroundImage: FileImage(_image),
+                        backgroundImage: FileImage(selectedImage!),
                         backgroundColor: Colors.transparent,
                       )
                     : CircleAvatar(
                         radius: MediaQuery.of(context).size.width / 5,
-                        backgroundImage: AssetImage('assets/img_default.png'),
+                        backgroundImage: const AssetImage('assets/img_default.png'),
                         backgroundColor: Colors.white60,
                       )),
             const SizedBox(height: 10,),
@@ -101,7 +98,11 @@ class _ImageUploadState extends State<ImageUpload> {
                   style: TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600,),
                 ),
                 onPressed: () => {
-                  uploadImage()
+                  if(isImageSelected == false){
+                    showAlert(context: context, title: "Error Image Upload", content: "No file was selected")
+                  } else {
+                    uploadImage()
+                  }
                 },
               ),
             ),
@@ -111,52 +112,37 @@ class _ImageUploadState extends State<ImageUpload> {
     );
   }
 
-  _imageFromCamera() async {
+  pickFromCamera() async {
     try {
       XFile? capturedImage = await imagePicker.pickImage(source: ImageSource.camera);
       final File imagePath = File(capturedImage!.path);
-      if (capturedImage == null) {
-        showAlert(bContext: context, title: "Error choosing file", content: "No file was selected");
-      } else {
-        setState(() {
-          _image = imagePath;
-          capture = true;
-
-        });
-        //Navigator.push(context, MaterialPageRoute(builder: (context) => DisplayPicture(image: _image, context: context,)));
-      }
-    } catch (e) {
-      showAlert(bContext: context, title: "Error capturing image file", content: e.toString());
-    }
-  }
-
-  _imageFromGallery() async {
-    XFile? uploadedImage = await imagePicker!.pickImage(source: ImageSource.gallery);
-    final File imagePath = File(uploadedImage!.path);
-
-    if (uploadedImage == null) {
-      showAlert(bContext: context, title: "Error choosing file", content: "No file was selected");
-    } else {
       setState(() {
-        _image = imagePath;
-        capture = true;
+        selectedImage = imagePath;
+        isImageSelected = true;
       });
-      //Navigator.push(context, MaterialPageRoute(builder: (context) => DisplayPicture(image: _image, context: context,)));
+    } catch (e) {
+      showAlert(context: context, title: "Error capturing image file", content: e.toString());
     }
   }
 
-  showAlert({BuildContext? bContext, String? title, String? content}) {
+  pickFromGallery() async {
+    XFile? uploadedImage = await imagePicker.pickImage(source: ImageSource.gallery);
+    final File imagePath = File(uploadedImage!.path);
+    setState(() {
+      selectedImage = imagePath;
+      isImageSelected = true;
+    });
+  }
+
+  showAlert({BuildContext? context, String? title, String? content}) {
     return showDialog(
-        context: bContext!,
+        context: context!,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: Text(
-              title ?? "",
-              style: const TextStyle(color: Colors.white),
-            ),
-            content: Text(content ?? ""),
+            title: Text(title! , style: const TextStyle(color: Colors.black87),),
+            content: Text(content! ),
             actions: [
-              TextButton(onPressed: () => {Navigator.pop(context)}, child: const Text("Okay"))
+              TextButton(onPressed: () => {Navigator.pop(context)}, child: const Text("OK"))
             ],
           );
         });
@@ -171,7 +157,7 @@ class _ImageUploadState extends State<ImageUpload> {
       await stickDirectory.create(recursive: true);
 
       // copy the file to a new path
-      File compressImg = await compressAndResizeImage(File(_image.path)).copy('$path/com_image1.png');
+      File compressImg = await ImageUtil().compressAndResizeImage(File(selectedImage!.path)).copy('$path/com_image1.png');
       // replace with your server url
       var urlInsertImage = "http://00.00.00.88:8989/manageProfile/addProfileImage";
       // replace with your auth token, if present, else no need to add token..
@@ -180,13 +166,16 @@ class _ImageUploadState extends State<ImageUpload> {
 
       var request = http.MultipartRequest("POST", Uri.parse(urlInsertImage));
       request.headers.addAll(headers);
-      request.files.add(http.MultipartFile.fromBytes("file", compressImg.readAsBytesSync(),filename: _image.path));
+      request.files.add(http.MultipartFile.fromBytes("file", compressImg.readAsBytesSync(),filename: selectedImage!.path));
       var response = await request.send();
 
       try {
         var res = await http.Response.fromStream(response);
         if (res.statusCode == 200) {
           debugPrint("SUCCESS! 200 HTTP");
+          // don't forget to reset value here..
+          isImageSelected = false;
+          selectedImage = null;
         }
       } catch (e, s) {
         debugPrint("$e __ $s");
@@ -197,41 +186,4 @@ class _ImageUploadState extends State<ImageUpload> {
     }
     return false;
   }
-
-
-  Future<bool> _requestPermission(Permission permission) async {
-    if (await permission.isGranted) {
-      return true;
-    } else {
-      var result = await permission.request();
-      if (result == PermissionStatus.granted) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-
-
-  File compressAndResizeImage(File file) {
-    img.Image? image = img.decodeImage(file.readAsBytesSync());
-    // Resize the image to have the longer side be 800 pixels
-    int width;
-    int height;
-    if (image!.width > image!.height) {
-      width = 800;
-      height = (image.height / image.width * 800).round();
-    } else {
-      height = 800;
-      width = (image.width / image.height * 800).round();
-    }
-    img.Image resizedImage = img.copyResize(image, width: width, height: height);
-    // Compress the image with JPEG format
-    List<int> compressedBytes = img.encodeJpg(resizedImage, quality: 75);  // Adjust quality as needed
-    // Save the compressed image to a file
-    File compressedFile = File(file.path.replaceFirst('.jpg', '_compressed.jpg'));
-    compressedFile.writeAsBytesSync(compressedBytes);
-    return compressedFile;
-  }
-
 }
